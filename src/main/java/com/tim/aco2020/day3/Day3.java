27e3fa36dff601cb30434f6846972acb5f091dc1
Tim Sawyer <tim.sawyer@bjss.com>
2020-12-03 09:05:08 +0000

    Day 3 Part 1 working, part 2 not

---
+++ src/main/java/com/tim/aco2020/day3/Day3.java
@@ -0,0 +1,39 @@
+package com.tim.aco2020.day3;
+
+public class Day3 {
+
+    final String[] inputArray;
+    final int width;
+
+
+    public Day3(String[] inputArray) {
+        this.inputArray = inputArray;
+        this.width = this.inputArray[0].length();
+    }
+
+    public long execute(int right, int down) {
+        StringBuilder encountered = new StringBuilder();
+        int horizontal = right;
+        for (int i=1; i<this.inputArray.length; i += down) {
+            String line = this.inputArray[i];
+
+            encountered.append(line.charAt(horizontal));
+
+            horizontal = horizontal + right;
+            while (horizontal >= width) {
+                horizontal -= width;
+            }
+        }
+
+        return encountered.toString().chars().filter(map -> map == '#').count();
+    }
+
+    public long executePart2() {
+        long answer = this.execute(1,1);
+        answer *= this.execute(3,1);
+        answer *= this.execute(5,1);
+        answer *= this.execute(7,1);
+        answer *= this.execute(1,2);
+        return answer;
+    }
+}
---
+++ src/test/java/com/tim/aoc2020/day3/Day3Tests.java
@@ -0,0 +1,66 @@
+package com.tim.aoc2020.day3;
+
+import com.tim.aco2020.day2.Day2;
+import com.tim.aco2020.day3.Day3;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class Day3Tests {
+
+    @Test
+    void testExampleInput() throws IOException, URISyntaxException {
+        URL url = this.getClass().getClassLoader().getResource("Day3Example.txt");
+        Path filePath = Paths.get(url.toURI());
+        List<String> testInput = Files.readAllLines(filePath);
+        String[] testInputArray = testInput.toArray(new String[0]);
+
+        Day3 day = new Day3(testInputArray);
+        assertEquals(7, day.execute(3, 1));
+    }
+
+    @Test
+    void testPuzzleInput() throws IOException, URISyntaxException {
+        URL url = this.getClass().getClassLoader().getResource("Day3.txt");
+        Path filePath = Paths.get(url.toURI());
+        List<String> testInput = Files.readAllLines(filePath);
+        String[] testInputArray = testInput.toArray(new String[0]);
+
+        Day3 day = new Day3(testInputArray);
+        //System.out.println(day.execute(3, 1));
+    }
+
+    @Test
+    void testExampleInputPart2() throws IOException, URISyntaxException {
+        URL url = this.getClass().getClassLoader().getResource("Day3Example.txt");
+        Path filePath = Paths.get(url.toURI());
+        List<String> testInput = Files.readAllLines(filePath);
+        String[] testInputArray = testInput.toArray(new String[0]);
+
+        Day3 day = new Day3(testInputArray);
+        assertEquals(336, day.executePart2());
+    }
+
+    @Test
+    void testPuzzleInputPart2() throws IOException, URISyntaxException {
+        URL url = this.getClass().getClassLoader().getResource("Day3.txt");
+        Path filePath = Paths.get(url.toURI());
+        List<String> testInput = Files.readAllLines(filePath);
+        String[] testInputArray = testInput.toArray(new String[0]);
+
+        Day3 day = new Day3(testInputArray);
+        long result = day.executePart2();
+        assertNotEquals(1441507470, result);
+        System.out.println(result);
+    }
+
+}
